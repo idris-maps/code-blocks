eb30aea6d8301bf93b2254ed3b537084b204d5a5
Anders <idris.maps@gmx.ch>
2020-08-17 19:42:29 +0200

    Prevent from-html to fail if no language

--- packages/from-html/src/index.ts
+++ packages/from-html/src/index.ts
@@ -18,6 +18,8 @@ const splitPre = (html: string): PreSplit[] =>
     })
   )
 
+const hasLanguage = (d: string) => d.includes('class="language-')
+
 const parsePreSplit = (languages: LanguagesToParse) =>
   ({ isPre, content }: PreSplit): Part => {
     if (!isPre) {
@@ -29,7 +31,9 @@ const parsePreSplit = (languages: LanguagesToParse) =>
     const [_, noCode] = content.split('<code')
     const [codeAttributes, ...rest] = noCode.split('>')
     const code = rest.join('>').split('</code>')[0]
-    const language = codeAttributes.split('class="language-')[1].split('"')[0]
+    const language = hasLanguage(codeAttributes)
+      ? codeAttributes.split('class="language-')[1].split('"')[0]
+      : ''
     if (parseAllLanguages(languages) || languages.includes(language)) {
       return {
         type: 'code',
