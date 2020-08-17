27328d7491851a9cb50c6d85f4e52dc855455dc4
Anders <idris.maps@gmx.ch>
2020-08-17 19:32:42 +0200

    Let charts take all available width

--- packages/charts/src/vega.ts
+++ packages/charts/src/vega.ts
@@ -1,9 +1,18 @@
 import { View, parse } from 'vega'
 import { compile } from 'vega-lite'
 
+const removeWidthAndHeight = (svg: string) => {
+  const [svgTag, ...rest] = svg.split('>')
+  const svgAttrs = svgTag.split(' ')
+  return [
+    svgAttrs.filter(d => !d.startsWith('width=') && !d.startsWith('height=')).join(' '),
+    ...rest,
+  ].join('>')
+}
+
 export const renderVega = async (json: any) => {
   const view = new View(parse(json))
-  return await view.toSVG()
+  return removeWidthAndHeight(await view.toSVG())
 }
 
 export const renderVegalite = async (json: any) => {
