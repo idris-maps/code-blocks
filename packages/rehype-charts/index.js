153d85d5b6b235e1f026b48780d9c49b52897394
Anders <idris.maps@gmx.ch>
2020-08-20 20:53:15 +0200

    Remove previous rehype packages

--- packages/rehype-charts/index.js
+++
@@ -1,4 +0,0 @@
-const rehypeWrapper = require('@code-blocks/rehype-wrapper')
-const charts = require('@code-blocks/charts')
-
-module.exports = rehypeWrapper(charts.default, charts.acceptedLanguages)
--- packages/rehype-wrapper/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Renderer } from '@code-blocks/types'
-import { Transformer } from 'unified'
-declare const _default: (renderer: Renderer, languages: string[]) => (tree: any) => Promise<Transformer> 
-export default _default;
